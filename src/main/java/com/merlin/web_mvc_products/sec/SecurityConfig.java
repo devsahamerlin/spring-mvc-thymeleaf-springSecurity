123f34b77c34e85a3e44e49b578f4eaaa4fa8a9f
Merlin Saha <devsahamerlin@gmail.com>
2025-06-05 15:17:09 +0400

    feat(sec): add spring secuty in momory user and role for basic auth

---
+++ src/main/java/com/merlin/web_mvc_products/sec/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.merlin.web_mvc_products.sec;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Primary
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        PasswordEncoder encoder = passwordEncoder();
+        System.out.println("=========");
+        String encodedPWD = encoder.encode("123456");
+        System.out.println(encodedPWD);
+        System.out.println("=========");
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(passwordEncoder().encode("1234")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder().encode("1234")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder().encode("1234")).roles("USER","ADMIN").build()
+        );
+    }
+}
